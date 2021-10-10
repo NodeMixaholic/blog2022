af1c127f7fd86edb8c2f06fcf3c3f90695af1205
Samuel Lord <sam@sllord.info>
2021-10-10 05:01:03 +0000

    add h4-h6

--- post.js
+++ post.js
@@ -8,6 +8,9 @@ function getText(path) {
 
 function parseMD(markdown) {
 	const htmlText = markdown
+		.replace(/^###### (.*$)/gim, '<h6>$1</h6>')
+		.replace(/^##### (.*$)/gim, '<h5>$1</h5>')
+		.replace(/^#### (.*$)/gim, '<h4>$1</h4>')
 		.replace(/^### (.*$)/gim, '<h3>$1</h3>')
 		.replace(/^## (.*$)/gim, '<h2>$1</h2>')
 		.replace(/^# (.*$)/gim, '<h1>$1</h1>')

80b1751e086cfebc6f73b235a623656d9bd173ad
Samuel Lord <sam@sllord.info>
2021-10-10 04:44:56 +0000

    Update post.js

--- post.js
+++ post.js
@@ -2,6 +2,25 @@ function getJSON(path) {
     return fetch(path).then(response => response.json());
 }
 
+function getText(path) {
+    return fetch(path).then(response => response.text());
+}
+
+function parseMD(markdown) {
+	const htmlText = markdown
+		.replace(/^### (.*$)/gim, '<h3>$1</h3>')
+		.replace(/^## (.*$)/gim, '<h2>$1</h2>')
+		.replace(/^# (.*$)/gim, '<h1>$1</h1>')
+		.replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
+		.replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
+		.replace(/\*(.*)\*/gim, '<i>$1</i>')
+		.replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
+		.replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
+		.replace(/\n$/gim, '<br />')
+
+	return htmlText.trim()
+}
+
 getJSON('settings.json').then(listingsJSON => {
     let parsed = listingsJSON;
     const blogName = parsed.pageTitle
@@ -14,17 +33,8 @@ let paramString = decodedURL.split('?')[1];
 let args = new URLSearchParams(paramString);
 
 let urlOfMarkdown = args.get("listingURL")
-let converter = new showdown.Converter();
-function getTextThenSetBody() {
-    fetch(urlOfMarkdown)
-    .then(function(response) {
-    response.text().then(function(text) {
-        let blogMD = blob.text();
-        setBody(blogMD)
-    })
-})
-}
 
-function setBody(md) {
-    document.getElementById("body").innerHTML = converter.makeHtml(md)
+async function setBody() {
+    let contents = await getText(urlOfMarkdown)
+    document.getElementById("body").innerHTML = parseMD(contents)
 }
